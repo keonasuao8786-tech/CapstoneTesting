e028d1364e2314b5f598b5bef2ef185fe7c56e07
keonasuao8786 <keon.asuao8786@stu.mtec.edu>
2026-04-22 13:32:09 -0600

    I hope this worked for gitignore

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -2,6 +2,6 @@ import { browser } from '@wdio/globals'
 
 export default class Page {
     open (path) {
-        return browser.url(`https://the-internet.herokuapp.com/${path}`)
+        return browser.url(`https://app.thecasework.com/`)
     }
 }
