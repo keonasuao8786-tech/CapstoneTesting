fa9ef9cf360167f732e8a2700f6a329be6c67f7b
keonasuao8786 <keon.asuao8786@stu.mtec.edu>
2026-04-24 14:31:11 -0600

    Not working; need to look look for good selectors

--- test/pageobjects/Dashboard.js
+++ test/pageobjects/Dashboard.js
@@ -5,6 +5,9 @@ class SecurePage extends Page {
     get primaryText () {
         return $('[class*="fui-Persona__primaryText"]');
     }
+    get title () {
+        return $('[class="fui-Text ___h37oep0 fk6fouc fod5ikn faaz57k flh3ekv fpgzoln f1w7gpdv f6juhto f1gl81tg f2jf649 f1aehjj5 f1xqy1su"]')[0];
+    }
 }
 
 export default new SecurePage();
--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -8,7 +8,16 @@ describe('My Login application', () => {
         await browser.pause(1000);
 
         await LoginPage.login();
-        await expect(SecurePage.primaryText).toHaveText(['MTECH-QA']);
+        await expect(SecurePage.title).toHaveText(['My Cases']);
     })
 })
 
+// describe('Dashboard Testing', () => {
+//     it('should should test the My Cases Table to make sure it is working properly', async () => {
+//         await LoginPage.open();
+//         await expect(LoginPage.inputUsername).toExist();
+
+//         await LoginPage.login();
+        
+//     })
+// })
