9c007521324bc6f21345eab645732b2b44bd0dd8
keonasuao8786 <keon.asuao8786@stu.mtec.edu>
2026-04-28 11:39:00 -0600

    28/4/2026

--- test/pageobjects/Dashboard.js
+++ test/pageobjects/Dashboard.js
@@ -2,9 +2,7 @@ import { $ } from '@wdio/globals'
 import Page from './CaseWork.js';
 
 class SecurePage extends Page {
-    get primaryText () {
-        return $('[class*="fui-Persona__primaryText"]');
-    }
+
 }
 
 export default new SecurePage();
--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -3,17 +3,6 @@ import LoginPage from '../pageobjects/login.js'
 import SecurePage from '../pageobjects/Dashboard.js'
 import MyCases from '../pageobjects/MyCases.js'
 
-// describe('My Login application', () => {
-//     it('should login with valid credentials', async () => {
-//         await LoginPage.open();
-//         await browser.pause(1000);
-
-//         await LoginPage.login();
-//         await browser.pause(1000)
-//         await expect(SecurePage.searchInput).toExist();
-//     })
-// })
-
 describe('Dashboard Testing', () => {
     it('should should test the columns in the My Cases Table to make sure they sort the table correctly', async () => {
         await LoginPage.open();
