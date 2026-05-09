6a1709ad328b68f440411714271459deb10d9863
Keon Asuao <KEON.ASUAO8786@stu.mtec.edu>
2026-05-09 13:06:17 -0600

    Upcoming Events started

--- test/pageobjects/UpcomingEvents.js
+++ test/pageobjects/UpcomingEvents.js
@@ -1,8 +1,13 @@
 import { $ } from '@wdio/globals'
 import Page from './CaseWork.js';
 
-class SecurePage extends Page {
-
+class UpCEvents extends Page {
+    get eventOne () {
+        return $(`[class*="___1k3u5ge"]:nth-of-type(1)`);
+    }
+    get timeSpan () {
+        return $(`[data-testid="upcoming-events-filter-dropdown"]`);
+    }
 }
 
-export default new SecurePage();
+export default new UpCEvents();
--- test/specs/UpcomingEventsTest.js
+++ test/specs/UpcomingEventsTest.js
@@ -1,9 +1,17 @@
 import { browser, expect } from '@wdio/globals'
 import CaseLogin from '../pageobjects/login.js'
-import SecurePage from '../pageobjects/Dashboard.js'
 import MyCases from '../pageobjects/MyCases.js'
 import SearchBar from '../pageobjects/SearchBar.js'
 import MyTasks from '../pageobjects/MyTasks.js'
+import UpCEvents from '../pageobjects/UpcomingEvents.js'
 
 describe('Upcoming Events Table Testing', () => {
+    it (`should edit the items in the Upcoming Events table`, async () => {
+        await CaseLogin.openPage();
+        await expect(CaseLogin.username).toExist();
+        
+        await CaseLogin.login();
+        await UpCEvents.eventOne.waitForExist();
+        await expect(UpCEvents.eventOne).toExist();
+    })
 })
