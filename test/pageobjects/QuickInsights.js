fd01b295bf6155c7149c2b6af724531c68a8569a
keonasuao8786 <keon.asuao8786@stu.mtec.edu>
2026-05-11 11:55:49 -0600

    My test isn't working and I have to present in 10 minutes

--- test/pageobjects/QuickInsights.js
+++ test/pageobjects/QuickInsights.js
@@ -2,7 +2,15 @@ import { $ } from '@wdio/globals'
 import Page from './CaseWork.js';
 
 class QuickInsights extends Page {
-
+    get timeSpan () {
+        return $('[data-testid="quick-insights-time-period-menu"]');
+    }
+    get viewMenu () {
+        return $('[data-testid="quick-insights-view-filter-menu"]');
+    }
+    get bTimeMeter () {
+        return $('[role="meter"]');
+    }
 }
 
 export default new QuickInsights();
--- test/specs/QuickInsightsTest.js
+++ test/specs/QuickInsightsTest.js
@@ -6,7 +6,7 @@ import SearchBar from '../pageobjects/SearchBar.js'
 import MyTasks from '../pageobjects/MyTasks.js'
 
 describe('Quick Insights Table Testing', () => {
-    it('should make a Billable task within the My Tasks table', async () => {
+    it('should make a Billable and Non-Billable task within the My Tasks table', async () => {
         await CaseLogin.openPage();
         await expect(CaseLogin.username).toExist();
         
@@ -45,13 +45,6 @@ describe('Quick Insights Table Testing', () => {
 
             await MyTasks.testDesc();
             await expect(MyTasks.textBox).toHaveValue(MyTasks.desc);
-
-            await MyTasks.dateSelect.click();
-            await expect(MyTasks.calendar).toBeDisplayed();
-            await expect(MyTasks.today).toBeDisplayed();
-
-            await MyTasks.datecolumn1.click();
-            await expect(MyTasks.calendar).not.toBeDisplayed();
             await expect(MyTasks.saveBtn).toBeClickable();
             await MyTasks.saveBtn.click();
 
@@ -63,6 +56,54 @@ describe('Quick Insights Table Testing', () => {
             await expect(MyTasks.addTaskWindow).not.toBeDisplayed();
         }
 
+        await MyTasks.popUpNotif.waitForExist({ timeout: 30000 });
+        await MyTasks.popUpNotif.click();
+        await MyTasks.popUpNotif.waitForExist({ timeout: 1000 }).catch(() => false);
+
+        await MyTasks.addTask.moveTo();
+        await MyTasks.addTask.click();
+        await MyTasks.addTaskWindow.waitForDisplayed();
+        await expect(MyTasks.addTaskWindow).toBeDisplayed();
+        
+        await MyTasks.title.waitForClickable();
+        await expect(MyTasks.saveBtn).not.toBeClickable();
+        await MyTasks.testTitle();
+        await expect(MyTasks.title).toHaveValue(MyTasks.titleV);
+        
+        await MyTasks.caseDropdown.click();
+        await MyTasks.listMenu.waitForDisplayed();
+        await expect(MyTasks.listMenu).toBeDisplayed();
+        
+        await MyTasks.optionOne.waitForDisplayed(true);
+        await MyTasks.optionOne.moveTo();
+        await MyTasks.optionOne.click();
+        await expect(MyTasks.listMenu).not.toBeDisplayed();
+        
+        await MyTasks.milestoneMenu.click();
+        await MyTasks.listMenu.waitForDisplayed();
+        await expect(MyTasks.listMenu).toBeDisplayed();
+        await MyTasks.optionOne.waitForDisplayed();
+        if (await MyTasks.optionOne.waitForExist({timeout: 1000})) {
+            await MyTasks.optionOne.click();
+            await expect(MyTasks.listMenu).not.toBeDisplayed();
+            await expect(MyTasks.listMenu).not.toBeDisplayed();
+        
+            await expect(MyTasks.billableF).toExist();
+        
+            await MyTasks.testDesc2();
+            await expect(MyTasks.textBox).toHaveValue(MyTasks.desc2);
+        
+            await expect(MyTasks.saveBtn).toBeClickable();
+            await MyTasks.saveBtn.click();
+        
+            await expect(MyTasks.addTaskWindow).not.toBeDisplayed();
+            await MyTasks.taskTwo.waitForDisplayed();
+            await expect(MyTasks.taskTwo).toExist();
+        } else {
+            await MyTasks.failMsg();
+            await expect(MyTasks.addTaskWindow).not.toBeDisplayed();
+        }
+        
         await MyTasks.popUpNotif.waitForExist({ timeout: 30000 });
         await MyTasks.popUpNotif.click();
         await MyTasks.popUpNotif.waitForExist({ timeout: 1000 }).catch(() => false);
